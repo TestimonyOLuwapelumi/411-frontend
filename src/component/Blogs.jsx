7f72c8a9eeb07a7e6859b67b12a85bf0b85b37ce
Adeoyetestimony <testimonyoluwapelumi@gmail.com>
2023-03-21 19:05:18 +0100

    i make some change at the logo

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -223,10 +223,10 @@ const Blogs = ({blogs, firstblogs}) => {
 				</a>
 				<div className="flex flex-col flex-1 p-6">
 					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400">Convenire</a>
+					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline">Convenire</a>
 					<h3 className="flex-1 py-2 text-lg font-semibold leading-snug ">Te nulla oportere reprimique his dolorum</h3>
 					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-						<span>June 1, 2020</span>
+						<span >June 1, 2020</span>
 						<span>2.1K views</span>
 					</div>
 				</div>
