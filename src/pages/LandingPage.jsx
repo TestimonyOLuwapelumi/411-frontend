2290a9baa4c361608849159475b329a79dba6ab1
Adeoyetestimony <testimonyoluwapelumi@gmail.com>
2023-04-18 19:44:14 +0100

    i added landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Landing, Navbar } from '../component'
+
+const LandingPage = () => {
+  return (
+    <>
+    <Navbar/>
+    <Landing/>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default LandingPage
