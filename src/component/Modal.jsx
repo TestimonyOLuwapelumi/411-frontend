b2d43a22b2197b8f1ae7ebb61b3dea7aafa310b8
Adeoyetestimony <testimonyoluwapelumi@gmail.com>
2023-07-12 09:48:27 +0100

    i make a change at the modal buttons

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -60,7 +60,7 @@ const Modal = () => {
 			<div class="modal-content py-4 text-left px-6">
 				<div class="flex justify-between items-center pb-3">
 					<p class="text-2xl font-bold"></p>
-          <img src={logoo} alt="logo" className='h-16 w-16'/>
+          <img src={logoo} alt="logo" className='h-16 w-16 '/>
 					<div class="modal-close cursor-pointer z-50">
             <button  onClick={closeModal} className='bg-white'>
 
@@ -76,12 +76,14 @@ const Modal = () => {
 				<div class="my-5">
 					<p>Are you looking for a creative community where you can interact with like-minds, read stories and make contributions by participating in monthly challenges? JOIN THE 411 SQUAD 👇</p>
 				</div>
-				<div class="flex justify-end pt-2">
+				<div class="flex justify-center pt-2">
 					<button
 						class="focus:outline-none modal-close px-3 bg-blue-500 p-2 rounded-lg text-black hover:bg-gray-300"  onClick={closeModal}>Cancel</button>
             <Link to={"https://t.me/+kIB0FR_73L1iMDU0"}>
 					<button
-						className="focus:outline-none px-3 text-xs lg:text-lg  bg-blue-800 p-2 ml-3 rounded-lg text-white hover:bg-teal-400 flex " onClick={closeModal}><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="mt-0 lg:mt-1 bi bi-telegram" viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/> </svg> <span className='mt-1'>Join us on Telegram</span>  </button>
+						className="focus:outline-none px-3 text-xs lg:text-lg  bg-blue-800 p-2 ml-3 rounded-lg text-white hover:bg-teal-400 flex " onClick={closeModal}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="mt-0 lg:mt-1 bi bi-telegram" viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/> </svg> 
+              <span className='mt-1'>Join us on Telegram</span>  </button>
             </Link>
 				</div>
 			</div>
